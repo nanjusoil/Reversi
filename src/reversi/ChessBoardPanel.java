4efc30890960e38db6d5de8c8e96081686951716
nanjusoil <nanjusoil@gmail.com>
2016-08-03 12:41:02 +0800

    added ChessBoardPanel

---
+++ src/reversi/ChessBoardPanel.java
@@ -0,0 +1,5 @@
+package reversi;
+
+public class ChessBoardPanel {
+    Chess[][] chess = new Chess[8][8];
+}
--- src/reversi/Main.java
+++ src/reversi/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     private final JPanel gui = new JPanel(new BorderLayout(3, 3));
     private final int counDownTime = 30;
-    private Chess[][] chess = new Chess[8][8];
+    ChessBoardPanel chessBoardPanel = new ChessBoardPanel();
     private JPanel chessBoard;
     private final JLabel message = new JLabel(
             "Chess Champ is ready to play!");
@@ -53,10 +53,10 @@ public class Main {
     }
 
     public final void initializeChess(){
-    	chess[3][3].dropBlack();
-    	chess[3][4].dropWhite();
-    	chess[4][3].dropWhite();
-    	chess[4][4].dropBlack();
+    	chessBoardPanel.chess[3][3].dropBlack();
+    	chessBoardPanel.chess[3][4].dropWhite();
+    	chessBoardPanel.chess[4][3].dropWhite();
+    	chessBoardPanel.chess[4][4].dropBlack();
     }
     
     public final void initializeGui() {
@@ -81,7 +81,7 @@ public class Main {
 			public void mousePressed(MouseEvent mouseEvent) {
 				currentState = Chess.BLACK;
 				timer.stop();
-				socketServer = new SocketServer(chess);
+				socketServer = new SocketServer(chessBoardPanel.chess);
 				Thread serverThread = new Thread(socketServer);
 				serverThread.start();
 			}
@@ -105,7 +105,7 @@ public class Main {
 			public void mousePressed(MouseEvent mouseEvent) {
 				currentState = Chess.WHITE;
 				timer.stop();
-				socketClient = new SocketClient(chess);
+				socketClient = new SocketClient(chessBoardPanel.chess);
 				Thread clientThread = new Thread(socketClient);
 				clientThread.start();
 			}
@@ -127,21 +127,21 @@ public class Main {
         gui.add(chessBoard);
 
         Insets buttonMargin = new Insets(0,0,0,0);
-        for (int i = 0; i < chess.length; i++) {
-            for (int j = 0; j < chess[i].length; j++) {
-            	chess[i][j] = new Chess();
-            	chess[i][j].jButton.setMargin(buttonMargin);
+        for (int i = 0; i < chessBoardPanel.chess.length; i++) {
+            for (int j = 0; j < chessBoardPanel.chess[i].length; j++) {
+            	chessBoardPanel.chess[i][j] = new Chess();
+            	chessBoardPanel.chess[i][j].jButton.setMargin(buttonMargin);
                 ImageIcon icon = new ImageIcon(
                         new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
-                chess[i][j].jButton.setIcon(icon);
-                chess[i][j].jButton.setBackground(Color.ORANGE);
+                chessBoardPanel.chess[i][j].jButton.setIcon(icon);
+                chessBoardPanel.chess[i][j].jButton.setBackground(Color.ORANGE);
             }
         }
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
             	initializeListener( i , j);
-                chessBoard.add(chess[j][i].jButton);
+                chessBoard.add(chessBoardPanel.chess[j][i].jButton);
             }
         }
     }
@@ -155,7 +155,7 @@ public class Main {
     }
 
     public void initializeListener(int i , int j){
-    	chess[i][j].jButton.addMouseListener(new MouseListener(){
+    	chessBoardPanel.chess[i][j].jButton.addMouseListener(new MouseListener(){
 					@Override
 					public void mouseClicked(MouseEvent mouseEvent) {}
 					@Override
@@ -166,9 +166,9 @@ public class Main {
 					public void mousePressed(MouseEvent mouseEvent) {
 						updateCanClick();
 						//System.out.println(i + " " + j);
-						if(chess[i][j].blackCanClick && currentState == Chess.BLACK){
+						if(chessBoardPanel.chess[i][j].blackCanClick && currentState == Chess.BLACK){
 							checkOrFlip(i , j , true);
-							chess[i][j].dropBlack();
+							chessBoardPanel.chess[i][j].dropBlack();
 							changState();
 							try {
 								if(socketServer!=null){
@@ -181,9 +181,9 @@ public class Main {
 								e.printStackTrace();
 							}
 							}
-						if(chess[i][j].whiteCanClick && currentState == Chess.WHITE){
+						if(chessBoardPanel.chess[i][j].whiteCanClick && currentState == Chess.WHITE){
 							checkOrFlip(i , j , true);
-							chess[i][j].dropWhite();
+							chessBoardPanel.chess[i][j].dropWhite();
 							changState();
 							try {
 								if(socketServer!=null){
@@ -206,8 +206,8 @@ public class Main {
     public void updateCanClick(){
     	for(int i = 0 ; i < 8 ; i++){
     		for(int j = 0 ; j < 8 ; j++){
-    			chess[i][j].blackCanClick = false;
-    			chess[i][j].whiteCanClick = false;
+    			chessBoardPanel.chess[i][j].blackCanClick = false;
+    			chessBoardPanel.chess[i][j].whiteCanClick = false;
     		}
     	}
     	for(int x = 0 ; x <8 ; x++){
@@ -221,37 +221,37 @@ public class Main {
     	int nextState = 0;
     	int[][] directions = { {-1 , 0} , {0 , -1} , {-1 , -1} , {1 , 1} , {1 , -1 } , {1 , 0} , {0 , 1} , {-1 , 1}};
     	
-		if(chess[x][y].getState() == Chess.BLANK){
+		if(chessBoardPanel.chess[x][y].getState() == Chess.BLANK){
 			for(int direction = 0 ; direction < 8 ; direction++){
 				if(((x + directions[direction][0] >=0) && (x + directions[direction][0]) <8) && ((y + directions[direction][1] >=0) && (y + directions[direction][1] <8)))
-					nextState = chess[x + directions[direction][0]][y + directions[direction][1]].getState();
+					nextState = chessBoardPanel.chess[x + directions[direction][0]][y + directions[direction][1]].getState();
 				ArrayList<int []> myList = new ArrayList<int []>();
 				for(int i = x + directions[direction][0] * 2 , j = y + directions[direction][1] * 2; (i + directions[direction][0] < 8) && (i + directions[direction][0] >= 0) && (j + directions[direction][1]) < 8 && (j + directions[direction][1]) >= 0; i = i + directions[direction][0] , j = j + directions[direction][1]){
 					if(i >=0 && i <8 && j >=0 && j<8){
-						if( chess[i][j].getState() == Chess.BLANK){
+						if( chessBoardPanel.chess[i][j].getState() == Chess.BLANK){
 							break;
 						}
 						int[] temp={i, j};
 						myList.add(temp);
-						if( chess[i][j].getState() != nextState){
+						if( chessBoardPanel.chess[i][j].getState() != nextState){
 							if(nextState == Chess.BLACK){
 
 								if(shouldFlip){
-									chess[x + directions[direction][0]][y + directions[direction][1]].dropWhite();
+									chessBoardPanel.chess[x + directions[direction][0]][y + directions[direction][1]].dropWhite();
 									for(int k = 0 ; k <myList.size() ; k++){
-										chess[myList.get(k)[0]][myList.get(k)[1]].dropWhite();
+										chessBoardPanel.chess[myList.get(k)[0]][myList.get(k)[1]].dropWhite();
 									}
 								}
-								chess[x][y].whiteCanClick = true;
+								chessBoardPanel.chess[x][y].whiteCanClick = true;
 							}
 							if(nextState == Chess.WHITE){
 								if(shouldFlip){
-									chess[x + directions[direction][0]][y + directions[direction][1]].dropBlack();
+									chessBoardPanel.chess[x + directions[direction][0]][y + directions[direction][1]].dropBlack();
 									for(int k = 0 ; k <myList.size() ; k++){
-										chess[myList.get(k)[0]][myList.get(k)[1]].dropBlack();
+										chessBoardPanel.chess[myList.get(k)[0]][myList.get(k)[1]].dropBlack();
 									}
 								}
-								chess[x][y].blackCanClick = true;
+								chessBoardPanel.chess[x][y].blackCanClick = true;
 							}
 							break;
 						}
@@ -260,8 +260,8 @@ public class Main {
 			}
 		}
 		else{
-			chess[x][y].blackCanClick = false;
-			chess[x][y].whiteCanClick = false;
+			chessBoardPanel.chess[x][y].blackCanClick = false;
+			chessBoardPanel.chess[x][y].whiteCanClick = false;
 		}
     	checkBoard();
     }
@@ -283,20 +283,20 @@ public class Main {
     public void checkBoard(){
     	for(int i = 0 ; i < 8 ; i++)
     		for(int j = 0 ; j < 8 ; j++){
-    			if(chess[i][j].getState() == Chess.BLANK)
-    				chess[i][j].initButton();
-    			if(chess[i][j].getState() == Chess.BLACK)
-    				chess[i][j].dropBlack();
-    			if(chess[i][j].getState() == Chess.WHITE)
-    				chess[i][j].dropWhite();
+    			if(chessBoardPanel.chess[i][j].getState() == Chess.BLANK)
+    				chessBoardPanel.chess[i][j].initButton();
+    			if(chessBoardPanel.chess[i][j].getState() == Chess.BLACK)
+    				chessBoardPanel.chess[i][j].dropBlack();
+    			if(chessBoardPanel.chess[i][j].getState() == Chess.WHITE)
+    				chessBoardPanel.chess[i][j].dropWhite();
     		}	
     			
     	for(int i = 0 ; i < 8 ; i++)
     		for(int j = 0 ; j < 8 ; j++){
-    			if(currentState == Chess.BLACK && chess[i][j].blackCanClick == true && chess[i][j].getState() == Chess.BLANK){
-    				chess[i][j].dropCanClick();
-    			}else if(currentState == Chess.WHITE && chess[i][j].whiteCanClick == true && chess[i][j].getState() == Chess.BLANK){
-    				chess[i][j].dropCanClick();
+    			if(currentState == Chess.BLACK && chessBoardPanel.chess[i][j].blackCanClick == true && chessBoardPanel.chess[i][j].getState() == Chess.BLANK){
+    				chessBoardPanel.chess[i][j].dropCanClick();
+    			}else if(currentState == Chess.WHITE && chessBoardPanel.chess[i][j].whiteCanClick == true && chessBoardPanel.chess[i][j].getState() == Chess.BLANK){
+    				chessBoardPanel.chess[i][j].dropCanClick();
     			}
     		}
     			
